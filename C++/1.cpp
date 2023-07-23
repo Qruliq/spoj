586487e8a9cfde5ec22c8defc90f706ae8cd37b4
Qruliq <137716873+Qruliq@users.noreply.github.com>
2023-07-23 22:42:28 +0200

    Create 1.cpp
    TEST - Life, the Universe, and Everything

---
+++ C++/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int i = 0;
+    
+int main() 
+{
+
+	
+	while(1)
+	{
+		cin >> i;
+		if(i == 42)break;
+		cout << i << "\n";
+	}
+
+	cin >> i;
+
+    return 0;
+}
